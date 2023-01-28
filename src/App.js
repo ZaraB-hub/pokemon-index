ead35b7670e9ee76a7b274f841de3fbb09ded42c
Zara <zarabahtanovic@gmail.com>
2023-01-28 21:14:36 +0100

    routing to pokedex.us

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { FcSearch } from "react-icons/fc";
 import { useEffect, useState } from 'react';
 import Card from './components/Card';
+import {BrowserRouter as Router} from 'react-router-dom'
 
 const API_URL="https://pokeapi.co/api/v2/pokemon/";
 
@@ -12,14 +13,24 @@ const[searchTerm,setSearchTerm]=useState("");
 const[pokemon,setPokemon]=useState({});
 
 
-const getPokemon=async(name)=>{
-  const response=await fetch(`${API_URL}${name}`);
-  const data=await response.json();
+const getPokemon=(name)=>{
+  try{
+  fetch(`${API_URL}${name}`)
+  .then(response=>response.json())
+  .then(data=>{
   console.log(data);
-  setPokemon(data);
+  setPokemon(data);})
+  }
+  catch(error){
+    if (error.message.includes("Not Found")) {
+      console.log("No Pokemon found with that name.");
+      // document.querySelector(".cards").innerHTML="<h1>No Pokemon found.</h1>";
+    } 
+  }
 }
 
   return (
+    <Router>
     <div className='container'>
         <img className='logo' src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/International_Pok%C3%A9mon_logo.svg/2560px-International_Pok%C3%A9mon_logo.svg.png" alt="" />
         <div className="search">
@@ -33,6 +44,7 @@ const getPokemon=async(name)=>{
           <Card pokemon={pokemon}/>
         </div>
     </div>
+    </Router>
   );
   }
 
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,23 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
-function Card({pokemon}) {
+function Card({ pokemon }) {
     return (
+    <Link to={`https://www.pokemon.com/us/pokedex/${pokemon.name}`}>
+
+        <div className="pokemon">
+
         <div>
-            <h1>POkemon</h1>
             <img src={pokemon.sprites.other['official-artwork'].front_default} alt="" />
         </div>
+
+        <div>
+            <span>{pokemon.name}</span>
+            <h4>Type: {pokemon.types.map(({type:{name}}) => name.charAt(0).toUpperCase() + name.slice(1) ).join("/")}</h4>
+            <h4>Abilities: {pokemon.abilities.map(({ability:{name}}) => name.charAt(0).toUpperCase() + name.slice(1) ).join(", ")}</h4>
+        </div>
+        </div>
+        </Link>
     )
 }
 
