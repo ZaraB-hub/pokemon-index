527d4da3cd176915a0e289bd4a61f50ecdef483d
Zara <zarabahtanovic@gmail.com>
2023-01-29 15:41:16 +0100

    displayAll

--- src/App.js
+++ src/App.js
@@ -12,53 +12,72 @@ function App() {
 
   const [searchTerm, setSearchTerm] = useState("");
   const [pokemon, setPokemon] = useState({});
+  const [pokemons, setPokemons] = useState([]);
 
   useEffect(() => {
     var input = document.querySelector('input[type="text"]');
     var elementToShow = document.getElementById('searchbutton');
 
-    input.addEventListener('focus', function() {
+    input.addEventListener('focus', function () {
       elementToShow.style.display = 'block';
     });
-    input.addEventListener('blur', function() {
-      setTimeout(function() {
+    input.addEventListener('blur', function () {
+      setTimeout(function () {
         elementToShow.style.display = 'none';
       }, 1);
     });
   }, []);
 
-  const getPokemon = (name) => {
-    try{
-    fetch(`${API_URL}${name}`)
-    .then(response=>response.json())
-    .then(data=>{
-    console.log(data);
-    setPokemon(data);})
-    }
-    catch(error){
-      if (error.message.includes("N")) {
-        console.log("No Pokemon found with that name.");
-        // document.querySelector(".cards").innerHTML="<h1>No Pokemon found.</h1>";
-      } 
-    }
+
+  useEffect(() => {
+    fetch(`https://pokeapi.co/api/v2/pokemon?limit=50&offset=0`)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data.results)
+        data.results.forEach(element => {
+          fetch(element.url).then(response => response.json())
+          .then(data => {console.log(data); setPokemons(prevPokemons => [...prevPokemons, data]);})
+        });
+        })
+  }, [])
+
+
+  const getPokemon = async(name) => {
+    const response=await fetch(`${API_URL}${name}`);
+    const data=await response.json();
+    setPokemon(data);
   }
 
   return (
     <Router>
-      <div className='container'>
-        <img className='logo' src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/International_Pok%C3%A9mon_logo.svg/2560px-International_Pok%C3%A9mon_logo.svg.png" alt="" />
+      <div className='big'>
+        <div className='container'>
+          <img className='logo' src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/International_Pok%C3%A9mon_logo.svg/2560px-International_Pok%C3%A9mon_logo.svg.png" alt="" />
 
-        <div className="search">
-          <input type="text" name="" id="" placeholder='Search for pokemon'
-            value={searchTerm}
-            onChange={(e) => setSearchTerm(e.target.value)}
-            onKeyDown={(e) => { if (e.key === 'Enter') getPokemon(searchTerm) }} />
-          <GoSearch id='searchbutton' style={{ color: 'grey' }} size={20} onClick={() => { getPokemon(searchTerm) }} />
+          <div className="search">
+            <input type="text" name="" id="" placeholder='Search for pokemon'
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
+              onKeyDown={(e) => { if (e.key === 'Enter') getPokemon(searchTerm) }} />
+            <GoSearch id='searchbutton' style={{ color: 'grey' }} size={20} onClick={() => { getPokemon(searchTerm) }} />
+          </div>
         </div>
 
+
+        {searchTerm ? (
+        <div className="card">
+          <Card pokemon={pokemon} />
+        </div>
+      ) : (
         <div className="cards">
-          <Card pokemon={pokemon}/>
+          {pokemons.map((pokemon) => (
+                  <Card pokemon={pokemon} />
+                ))}
         </div>
+      )}
+
+        
+
       </div>
 
 
